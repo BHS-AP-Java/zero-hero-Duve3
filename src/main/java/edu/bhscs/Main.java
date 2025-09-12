c99c32ee6cd29119a733b0db3730e4421dccbf79
Dhruv Hooda <Duv3tabest@gmail.com>
2025-09-12 16:45:54 +0000

    Flipping packet yo

--- src/main/java/edu/bhscs/Main.java
+++ src/main/java/edu/bhscs/Main.java
@@ -1,13 +1,23 @@
-// Student name
-// class period
-// project name
-// date
+// Dhruv H
+// P2
+// Zero Hero
+// 9/12/25
 
 /*
- * DESCRIPTION:
- * INPUT:
- * OUTPUT:
- * EDGE CASE:
+ * DESCRIPTION: whatever mr reiber says
+ * INPUT: We don't know about input yet
+ * OUTPUT: Some cool words in the command line
+ * EDGE CASE: If you run it on a machine wtih less than 0 mb of ram it will crash
  */
 
 package edu.bhscs;
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("--------------------------");
+    System.out.println("!!!! PROGRAM START !!!!");
+    System.out.println("Flippnig packet yo!");
+    System.out.println("!!!! PROGRAM END   !!!!");
+
+  }
+}
